7d8c153136d8fc9ba43cae1c60d3a6753bf0b8e0
Alexander Stepanov <vaiol.ans@gmail.com>
2017-03-10 17:41:49 +0200

    norminett'ed handle/*

--- libft/ft_printf/put_conversion/handle/handle.h
+++ libft/ft_printf/put_conversion/handle/handle.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astepano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/10 17:37:44 by astepano          #+#    #+#             */
+/*   Updated: 2017/03/10 17:41:18 by astepano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HANDLE_H
-#define HANDLE_H
+# define HANDLE_H
 
 # include "../util/util.h"
 
 char	*handle_minwidth(t_conversion *conv, char *str, size_t margin);
-char	*handle_precision(t_conversion *conv, char *str, int len, size_t margin);
+char	*handle_precision(t_conversion *conv, char *str, int l, size_t margin);
 char	*handle_apostrophe(char *nbr);
 
 #endif
--- libft/ft_printf/put_conversion/handle/handle_precision.c
+++ libft/ft_printf/put_conversion/handle/handle_precision.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_precision.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astepano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/10 17:38:13 by astepano          #+#    #+#             */
+/*   Updated: 2017/03/10 17:40:02 by astepano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "handle.h"
 
-char		*handle_precision(t_conversion *conv, char *str, int len, size_t margin)
+char	*handle_precision(t_conversion *conv, char *str, int l, size_t margin)
 {
 	int	count;
 
-	count = conv->precision - ((int)ft_strlen(str) - len);
+	count = conv->precision - ((int)ft_strlen(str) - l);
 	if (count > 0)
 		str = strjoinchr_begin(str, '0', count, margin);
 	return (str);
