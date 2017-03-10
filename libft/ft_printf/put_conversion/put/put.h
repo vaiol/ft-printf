2ae76eb392223983c9fb6bb5c3a34270a2663e80
Alexander Stepanov <vaiol.ans@gmail.com>
2017-03-10 17:43:33 +0200

    norminett'ed put/*

--- libft/ft_printf/put_conversion/put/put.h
+++ libft/ft_printf/put_conversion/put/put.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astepano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/10 17:42:28 by astepano          #+#    #+#             */
+/*   Updated: 2017/03/10 17:42:44 by astepano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUT_H
 # define PUT_H
 
-#include <unistd.h>
+# include <unistd.h>
 
-size_t			g_count;
+size_t	g_count;
 
-int				printed_count(void);
-void			ft_write(int fildes, const void *buf, size_t nbyte);
-void			putstr(char const *s);
+int		printed_count(void);
+void	ft_write(int fildes, const void *buf, size_t nbyte);
+void	putstr(char const *s);
 
 #endif
