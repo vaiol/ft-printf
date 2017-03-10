b74a1bdc4a531fb8945dd1203f80e3bb2f741693
Alexander Stepanov <vaiol.ans@gmail.com>
2017-03-10 17:47:43 +0200

    norminett'ed util/*

--- libft/ft_printf/put_conversion/util/util.h
+++ libft/ft_printf/put_conversion/util/util.h
@@ -1,19 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   util.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astepano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/10 17:44:27 by astepano          #+#    #+#             */
+/*   Updated: 2017/03/10 17:47:15 by astepano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UTIL_H
 # define UTIL_H
 
 # include "../../ft_printf.h"
 
-typedef enum    e_position
+typedef enum	e_position
 {
 	BEGIN,
 	END
-}               t_position;
+}				t_position;
 
-char			*utoa_base(unsigned long long value, int b, char size, int hash);
+char			*utoa_base(unsigned long long value, int b, char size,
+		int hash);
 char			*winttostr(wint_t wint);
 char			*wstrtostr(wchar_t *s, int max_len);
-char			*strjoinchr(char *str, char const chr, int count, t_position position);
-char			*strjoinchr_begin(char *str, char const chr, int count, size_t margin);
+char			*strjoinchr(char *str, char const chr, int count,
+		t_position position);
+char			*strjoinchr_begin(char *str, char const chr, int count,
+		size_t margin);
 char			*strclear(char *str);
 
 #endif
