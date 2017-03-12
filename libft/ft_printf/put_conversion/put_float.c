3d7824f35aeec7d9e7d6eb185cd0945899fb85b5
Alexander Stepanov <vaiol.ans@gmail.com>
2017-03-13 01:55:22 +0200

    some structure change

--- libft/ft_printf/put_conversion/put_float.c
+++ libft/ft_printf/put_conversion/put_float.c
@@ -12,7 +12,15 @@
 
 #include "put_conversion.h"
 
-void	put_float(t_conversion *conv, va_list valist)
+static char	*handle_float(long double nbr, int precision, char type)
+{
+	char	tmp[1000];
+
+	ftoa(tmp, nbr, precision, type);
+	return (ft_strdup(tmp));
+}
+
+void		put_float(t_conversion *conv, va_list valist)
 {
 	size_t	margin;
 	char	*str;
@@ -20,9 +28,9 @@ void	put_float(t_conversion *conv, va_list valist)
 	if (conv->precision < 0)
 		conv->precision = 6;
 	if (conv->ldouble)
-		str = handle_double(va_arg(valist, long double), conv->precision, conv->type);
+		str = handle_float(va_arg(valist, long double), conv->precision, conv->type);
 	else
-		str = handle_double(va_arg(valist, double), conv->precision, conv->type);
+		str = handle_float(va_arg(valist, double), conv->precision, conv->type);
 	margin = (size_t)(conv->sign || str[0] == '-');
 	if (conv->sign && str[0] != '-')
 		str = strjoinchr(str, conv->sign, 1, BEGIN);
